890cfefcec0558934813f8658f7be2569e25fee9
Jônathas Batista Silva <jonathasx57@gmail.com>
2021-11-19 22:59:08 -0300

    visual upgrade

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function funcCriar(){
     let tarefa = prompt("Digite a nova tarefa:");
 }
+
+function funcDel(){
+    let apagar = confirm("Realmente deseja apagar?");
+}
+
+function funcEdit(){
+    let editar = prompt("Digite o novo nome da tarefa:");
+}
